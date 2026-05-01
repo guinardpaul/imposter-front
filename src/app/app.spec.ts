96e02b98c1d3991291680c62388435095715b20d
guinardpaul <guinardpaul@gmail.com>
2026-05-01 12:06:53 +0200

    Add service for connecting to stomp server (#12)
    * Add service for connecting to stomp server
    
    * Fix linting problems
    
    * Setup test for web-socket-service.
    Setup environments files
    
    * add materials
    Init stomp connection
    Start implementing a lobby page and subscribing to game info updates
    
    * Fix linting
    
    * Fix angular/material version
    
    * fix room model

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { STOMP_CLIENT_FACTORY } from './core/ws/web-socket-token';
+import { Client } from '@stomp/stompjs';
 
 describe('App', () => {
+
+  let clientMock: Partial<Client>;
+  let factoryMock: () => Client;
+  
   beforeEach(async () => {
+
+    clientMock = {
+      configure: vi.fn(),
+      activate: vi.fn(),
+      onConnect: undefined
+    };
+
+    factoryMock = vi.fn(() => clientMock as Client);
+
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        {
+          provide: STOMP_CLIENT_FACTORY,
+          useValue: factoryMock
+        }
+      ]
     }).compileComponents();
   });
 
---
+++ src/app/core/ws/web-socket-service.ts
@@ -0,0 +1,43 @@
+import { Injectable, inject } from '@angular/core';
+import { Client, IMessage } from '@stomp/stompjs';
+import * as uuid from 'uuid';
+import { STOMP_CLIENT_FACTORY } from './web-socket-token';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WebSocketService {
+
+  private client!: Client;
+  private playerId = uuid.v4();
+
+  private clientFactory = inject(STOMP_CLIENT_FACTORY);
+
+  connect() {
+    this.client = this.clientFactory();
+
+    this.client.configure({
+      brokerURL: environment.ws.brokerURL,
+      reconnectDelay: 5000,
+      connectHeaders: {
+        "player-id": this.playerId
+      },
+      debug: (str) => console.log(str)
+    });
+    this.client.onConnect = () => {
+      console.log('Connected');
+    };
+
+    this.client.activate();
+  } 
+
+  subscribe(destination: string, callback: (message: IMessage) => void) {
+    this.client.subscribe(destination, callback);
+  }
+
+  publish(destination: string, body: unknown) {
+    this.client.publish({ destination, body: JSON.stringify(body) });
+  }
+
+}
